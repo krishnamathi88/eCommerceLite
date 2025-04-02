cfe680b9da5160728f3cecee9e7256f3faf2832c
krishnamathi88 <54958210+krishnamathi88@users.noreply.github.com>
2025-04-02 18:55:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 // Basic setup without compression
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/*', (req, res) => {
+app.get('(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'cb.html'));
 });
 
