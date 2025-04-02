4df4596d40372085560e60c4360699729b2d814b
krishnamathi88 <54958210+krishnamathi88@users.noreply.github.com>
2025-04-02 18:53:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const PORT = process.env.PORT || 3000;
 
 // Basic setup without compression
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'cb.html'));
 });
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
